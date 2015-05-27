fe37e3d7d4ab556f1371b323b5d4255565cae64c
Caio Filipe <caiofilipemr@gmail.com>
2015-05-27 00:35:45 -0300

    Testes em Engine!

--- Game/engine.cpp
+++ Game/engine.cpp
@@ -2,4 +2,6 @@
 
 Engine::Engine()
 {
+    Stalker my_stalker(2, 90, 10, 5, 12, 1, 1, "monster.png", SLEEP);
+    mobs[0] = my_stalker;
 }
--- Game/engine.h
+++ Game/engine.h
@@ -6,11 +6,19 @@
 #include "Character/Monster/sleeper.h"
 #include "Character/Monster/walker.h"
 #include "Character/Player/player.h"
+#include "stone.h"
+#include "chest.h"
+#include "globalfunctions.h"
+#include "Enums.h"
 
 class Engine
 {
 private:
-
+    Map my_map;
+    Monster mobs[5];
+    Player my_player;
+    Stone * my_stones;
+    Chest * my_chests;
 public:
     Engine();
 };
