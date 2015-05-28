7c73d7443d9b86802db9a520be2be6cc928b1346
Caio Filipe <caiofilipemr@gmail.com>
2015-05-28 16:47:29 -0300

    add mini metodo update em engine

--- Game/engine.cpp
+++ Game/engine.cpp
@@ -6,6 +6,11 @@ Engine::Engine()
     my_map = new Map("mapa.txt", "roguelikeSheet_transparent.png");
 }
 
+void Engine::update()
+{
+    my_player->walk();
+}
+
 void Engine::setPlayerDirection(Direction new_direction)
 {
     my_player->setDirection(new_direction);
