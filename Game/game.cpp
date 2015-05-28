8da61fc64ff4d242fbe7be7c0fb41a6714ead3de
Savio Cordeiro <savio.cordeiro@hotmail.com>
2015-05-28 16:01:01 -0300

    adicionado eventos do teclado

--- Game/game.cpp
+++ Game/game.cpp
@@ -8,9 +8,45 @@ Game::Game(QWidget *parent) :
     ui(new Ui::Game)
 {
     ui->setupUi(this);
+    clock = new QTimer(this);
+    clock->setInterval(16);
+    connect(clock, SIGNAL(timeout()), this, SLOT(myUpdate()));
+    clock->start();
+    atual_direction = SLEEP;
 }
 
 Game::~Game()
 {
     delete ui;
+}
+
+void Game::keyPressEvent(QKeyEvent *event)
+{
+    switch (event->key()) {
+    case Qt::Key_Up:
+       atual_direction = UP;
+        break;
+    case Qt::Key_Down:
+       atual_direction = DOWN;
+        break;
+    case Qt::Key_Left:
+       atual_direction = LEFT;
+        break;
+    case Qt::Key_Right:
+       atual_direction = RIGHT;
+        break;
+    }
+}
+
+void Game::keyReleaseEvent(QKeyEvent * event)
+{
+    atual_direction = SLEEP;
+}
+
+void Game::myUpdate()
+{
+    cerr << atual_direction;
+    my_engine.setPlayerDirection(atual_direction);
+    my_engine.update();
+    cerr << "Player- I =" <<my_engine.getPlayerCordenates().i<<" J =" << my_engine.getPlayerCordenates().j;
 }
--- Game/game.h
+++ Game/game.h
@@ -3,10 +3,13 @@
 
 #include <QMainWindow>
 #include <iostream>
+#include <QTimer>
+#include <QKeyEvent>
 
 #include "Character/Player/Raca/Elf/archer.h"
 #include "Character/Player/Raca/Elf/mage.h"
 #include "Character/Player/Raca/Orc/troll.h"
+#include "engine.h"
 //savio teste
 
 namespace Ui {
@@ -20,9 +23,19 @@ class Game : public QMainWindow
 public:
     explicit Game(QWidget *parent = 0);
     ~Game();
+    void keyPressEvent(QKeyEvent * event);
+    void keyReleaseEvent(QKeyEvent * event);
+
+public slots:
+    void myUpdate();
+
 
 private:
     Ui::Game *ui;
+    QTimer *clock;
+    Engine my_engine;
+    Direction atual_direction;
+
 };
 
 #endif // GAME_H
