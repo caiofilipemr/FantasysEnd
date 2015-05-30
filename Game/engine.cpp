c89a5c49e71bdc388580ee4d0e2d8a5f36f3769d
Caio Filipe <caiofilipemr@gmail.com>
2015-05-30 00:04:04 -0300

    Modificado isBattle para uma forma zoada mas que funciona!

--- Game/engine.cpp
+++ Game/engine.cpp
@@ -10,13 +10,13 @@ Engine::Engine()
     CellArray::instance()->setCell(my_map->getCordenates().i, my_map->getCordenates().j, my_map->getColision());
     mobs = new Monster*[number_of_mobs];
     mobs[0] = new Stalker(10, 10, 10, 10, 1, 90, 6, 5, 3, 2, 1, "", DOWN);
+    mobs[0]->setStalk(my_player);
 }
 
 void Engine::update()
 {
     try {
         if (my_map->hasColision(my_player->getCordenates() + my_player->getDirection())) my_player->update();
-        mobs[0]->setStalk(my_player);
         mobs[0]->setDirection(DOWN);
         mobs[0]->setCanGo(my_map->getCanGo(mobs[0]->getCordenates()));
         mobs[0]->update();
@@ -40,10 +40,27 @@ Cordenates Engine::getTemp()
 
 bool Engine::isBattle()
 {
+    //Arrumar uma solução MELHOR!!!
     for (int i = 0; i < number_of_mobs; i++) {
         if (mobs[i]) {
-            if ((mobs[i]->getCordenates() + mobs[i]->getEyeDirection()) == my_player->getCordenates()) {
+            if ((mobs[i]->getCordenates() + UP) == my_player->getCordenates()) {
                 battle_mob = mobs[i];
+                mobs[i]->setEyeDirection(UP);
+                return true;
+            }
+            if ((mobs[i]->getCordenates() + DOWN) == my_player->getCordenates()) {
+                battle_mob = mobs[i];
+                mobs[i]->setEyeDirection(DOWN);
+                return true;
+            }
+            if ((mobs[i]->getCordenates() + RIGHT) == my_player->getCordenates()) {
+                battle_mob = mobs[i];
+                mobs[i]->setEyeDirection(RIGHT);
+                return true;
+            }
+            if ((mobs[i]->getCordenates() + LEFT) == my_player->getCordenates()){
+                battle_mob = mobs[i];
+                mobs[i]->setEyeDirection(LEFT);
                 return true;
             }
         }
